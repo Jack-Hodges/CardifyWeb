a1b4dbddfe1899022e570999137b9a590eada422
Jack Hodges <jackhodg3s@gmail.com>
2024-09-13 11:12:21 +1000

    Added more icons

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -182,8 +182,8 @@ function SubjectBlock({ bgCol, subject, onEdit, onRemoveSubject }) {
             onMouseLeave={() => setHoveredIcon(null)}
             onClick={handlePracticeClick} // Trigger practice
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 sm:size-10">
-              <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8 sm:size-10">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clip-rule="evenodd" />
             </svg>
             {hoveredIcon === 'play' && (
               <div className="absolute -top-8 left-1/2 -translate-x-1/2 px-2 py-1 bg-black bg-opacity-50 text-white rounded-md text-sm transition-opacity duration-300 opacity-100">
--- src/components/Navigation/TitleBar.js
+++ src/components/Navigation/TitleBar.js
@@ -17,6 +17,33 @@ function TitleBar( { text }) {
         setIsOverlayVisible(!isOverlayVisible);
     };
 
+    // images
+
+    const dashBoard = (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+        <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
+      </svg>
+    )
+
+    const create = (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+        <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
+      </svg>
+    )
+
+    const practice = (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clip-rule="evenodd" />
+      </svg>
+    )
+
+    const home = (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+        <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+        <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+      </svg>
+    )
+
     return (
         <div className="relative">
             <div className="flex w-full text-left text-5xl font-bold text-gray-500 dark:text-gray-200 pl-5 items-center gap-2">
@@ -31,16 +58,16 @@ function TitleBar( { text }) {
                 <ul className="space-y-2">
                   {/* Use <Link> for navigation */}
                   <li><Link to="/" className="text-blue-500 hover:underline">
-                    <BackgroundButton text="Home" bgColor="green"/>
+                    <BackgroundButton image={home} flip text="Home" bgColor="green"/>
                   </Link></li>
                   <li><Link to="/dashboard" className="text-blue-500 hover:underline">
-                    <BackgroundButton text="Dashboard" bgColor="green"/>
+                    <BackgroundButton image={dashBoard} text="Dashboard" flip bgColor="green"/>
                   </Link></li>
                   <li><Link to="/create" className="text-blue-500 hover:underline">
-                    <BackgroundButton text="Create" bgColor="green"/>
+                    <BackgroundButton image={create} flip text="Create" bgColor="green"/>
                   </Link></li>
                   <li><Link to="/practice" className="text-blue-500 hover:underline">
-                    <BackgroundButton text="Practice" bgColor="green"/>
+                    <BackgroundButton image={practice} flip text="Practice" bgColor="green"/>
                   </Link></li>
                 </ul>
               </div>
