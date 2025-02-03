af08cbd0f69000afbc9906841a58f389ac247348
Jack Hodges <jackhodg3s@gmail.com>
2025-02-03 15:40:38 +1100

    Extracted buttons as components for cleaner code in EditModal

--- src/components/Card/EditModal.js
+++ src/components/Card/EditModal.js
@@ -33,6 +33,29 @@ function EditModal({
   // File for the back side (or front if you want to expand that logic)
   const [selectedFile, setSelectedFile] = useState(null);
 
+  const svgs = {
+    text: (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+      </svg>
+    ),
+    math: (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z" />
+      </svg>
+    ),
+    image: (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+      </svg>
+    ),
+    drawing: (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
+      </svg>
+    )
+  }
+
   useEffect(() => {
 
     if (clear) {
@@ -195,60 +218,12 @@ function EditModal({
 
           {/* Formatting Buttons (only active in "text" mode) */}
           <div className="mb-2 flex space-x-2">
-            <button
-              onClick={() => handleBoldClick('front')}
-              disabled={frontMode !== 0}
-              className={`bg-gray-100 w-8 h-8 rounded-md hover:bg-gray-200 ${
-                frontMode !== 0 ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
-            >
-              <b>B</b>
-            </button>
-            <button
-              onClick={() => handleItalicClick('front')}
-              disabled={frontMode !== 0}
-              className={`bg-gray-100 w-8 h-8 rounded-md hover:bg-gray-200 ${
-                frontMode !== 0 ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
-            >
-              <i>I</i>
-            </button>
-            <button
-              onClick={() => handleUnderlineClick('front')}
-              disabled={frontMode !== 0}
-              className={`bg-gray-100 w-8 h-8 rounded-md hover:bg-gray-200 ${
-                frontMode !== 0 ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
-            >
-              <u>U</u>
-            </button>
-
-            <button
-              onClick={() => setFrontMode(0)}
-              className={`bg-gray-100 w-[4rem] h-8 rounded-md hover:bg-gray-200 ${
-                frontMode === 0 ? 'bg-green-200' : ''
-              }`}
-            >
-              <div className="flex items-center justify-between space-x-2 px-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
-                </svg>
-                Text
-              </div>
-            </button>
-            <button
-              onClick={() => setFrontMode(1)}
-              className={`bg-gray-100 w-18 h-8 rounded-md hover:bg-gray-200 ${
-                frontMode === 1 ? 'bg-green-200' : ''
-              }`}
-            >
-            <div className="flex items-center justify-between space-x-2 px-1">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z" />
-              </svg>
-              Math
-            </div>
-            </button>
+            <TextButton text={<b>B</b>} handleClick={handleBoldClick} mode={frontMode} modeText="front" />
+            <TextButton text={<i>I</i>} handleClick={handleItalicClick} mode={frontMode} modeText="front" />
+            <TextButton text={<u>U</u>} handleClick={handleUnderlineClick} mode={frontMode} modeText="front" />
+            
+            <EditButton mode={frontMode} setMode={setFrontMode} svg={svgs.text} text="Text" val={0} extend/>
+            <EditButton mode={frontMode} setMode={setFrontMode} svg={svgs.math} text="Math" val={1} extend/>
           </div>
 
           {/* Conditionally render based on frontMode */}
@@ -307,73 +282,14 @@ function EditModal({
           </label>
 
           <div className="mb-2 flex space-x-2">
-            <button
-              onClick={() => handleBoldClick('back')}
-              disabled={backMode !== 0}
-              className={`bg-gray-100 w-8 h-8 rounded-md hover:bg-gray-200 ${
-                backMode !== 0 ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
-            >
-              <b>B</b>
-            </button>
-            <button
-              onClick={() => handleItalicClick('back')}
-              disabled={backMode !== 0}
-              className={`bg-gray-100 w-8 h-8 rounded-md hover:bg-gray-200 ${
-                backMode !== 0 ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
-            >
-              <i>I</i>
-            </button>
-            <button
-              onClick={() => handleUnderlineClick('back')}
-              disabled={backMode !== 0}
-              className={`bg-gray-100 w-8 h-8 rounded-md hover:bg-gray-200 ${
-                backMode !== 0 ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
-            >
-              <u>U</u>
-            </button>
-
-            <button
-              onClick={() => setBackMode(0)}
-              className={`bg-gray-100 w-[4rem] h-8 rounded-md hover:bg-gray-200 ${
-                backMode === 0 ? 'bg-green-200' : ''
-              }`}
-            >
-              <div className="flex items-center justify-between space-x-2 px-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
-                </svg>
-                Text
-              </div>
-            </button>
-            <button
-              onClick={() => setBackMode(1)}
-              className={`bg-gray-100 w-18 h-8 rounded-md hover:bg-gray-200 ${
-                backMode === 1 ? 'bg-green-200' : ''
-              }`}
-            >
-            <div className="flex items-center justify-between space-x-2 px-1">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z" />
-              </svg>
-              Math
-            </div>
-            </button>
-            <button
-              onClick={() => setBackMode(2)}
-              className={`bg-gray-100 w-20 h-8 rounded-md hover:bg-gray-200 ${
-                backMode === 2 ? 'bg-green-200' : ''
-              }`}
-            >
-              <div className="flex items-center justify-between space-x-2 px-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                </svg>
-                Image
-              </div>
-            </button>
+            <TextButton text={<b>B</b>} handleClick={handleBoldClick} mode={backMode} modeText="back" />
+            <TextButton text={<i>I</i>} handleClick={handleItalicClick} mode={backMode} modeText="back" />
+            <TextButton text={<u>U</u>} handleClick={handleUnderlineClick} mode={backMode} modeText="back" />
+            
+            <EditButton mode={backMode} setMode={setBackMode} svg={svgs.text} text="Text" val={0} extend/>
+            <EditButton mode={backMode} setMode={setBackMode} svg={svgs.math} text="Math" val={1} extend/>
+            <EditButton mode={backMode} setMode={setBackMode} svg={svgs.image} text="Image" val={2} />
+            <EditButton mode={backMode} setMode={setBackMode} svg={svgs.drawing} text="Draw" val={3} />
           </div>
 
           {backMode === 1 && (
@@ -441,3 +357,33 @@ function EditModal({
 }
 
 export default EditModal;
+
+function EditButton({mode, setMode, svg, text, val, extend}) {
+  return (
+    <button
+      onClick={() => setMode(val)}
+      className={`bg-gray-100 ${extend ? 'w-[4rem]' : 'w-20'} h-8 rounded-md hover:bg-gray-200 ${
+        mode === val ? 'bg-gray-300' : ''
+      }`}
+    >
+      <div className="flex items-center justify-around space-x-2 px-1">
+        {svg}
+        {text}
+      </div>
+    </button>
+  );
+}
+
+function TextButton({text, handleClick, mode, modeText}) {
+  return (
+    <button
+      onClick={() => handleClick(modeText)}
+      disabled={mode !== 0}
+      className={`bg-gray-100 w-8 h-8 rounded-md hover:bg-gray-200 ${
+        mode !== 0 ? 'opacity-50 cursor-not-allowed' : ''
+      }`}
+    >
+      {text}
+    </button>
+  );
+}
