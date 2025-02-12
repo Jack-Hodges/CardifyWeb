c6b24fc950248851c507eff233eee053a60d3959
Jack Hodges <jackhodg3s@gmail.com>
2025-02-12 12:02:50 +1100

    Enhance CollectionBlock component with backdrop blur effect for improved visual appeal

--- src/components/Collections/CollectionBlock.js
+++ src/components/Collections/CollectionBlock.js
@@ -27,7 +27,7 @@ function CollectionBlock({ user, collection, subjects, isExpanded = false, onCli
     return (
         <>
             {/* Closed collection box for the grid */}
-            <div className={`group relative mx-auto w-full h-56 cursor-pointer background-hover bg-[#f2f1ed] dark:bg-gray-700 rounded-xl background-shadow-new transition duration-300`}
+            <div className={`group relative mx-auto w-full h-56 cursor-pointer background-hover backdrop-blur-md rounded-xl background-shadow-new transition duration-300`}
             onClick={onClick}>
                 {/* Eight squares in the background */}
                 <div className="absolute inset-0 grid grid-cols-4 grid-rows-2 gap-2 py-6 px-5">
