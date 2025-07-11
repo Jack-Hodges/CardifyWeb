82ffbf8bbe46968ae6bd01c2c36b33472fae02be
Jack Hodges <jackhodg3s@gmail.com>
2025-07-11 11:33:58 +1000

    Update App and Home components for improved layout consistency

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -174,7 +174,7 @@ function Home() {
       <div className={`${shadow ? 'fixed top-0 left-0 w-full h-2/5 bg-gradient-to-b from-[rgba(0,0,0,0.2)] to-transparent z-5 pointer-events-none' : ''}`}></div>
 
       {/* Scrolling content */}
-      <div className="relative z-10 min-h-screen overflow-auto pb-20">
+      <div className="relative z-10 min-h-screen pb-20">
         <div>
           <TitleBar text="Home" user={user} home={true}/>
         </div>
