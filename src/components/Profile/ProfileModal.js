80ae1f1bd96da6a50e56e3bc774901e485c00892
Jack Hodges <jackhodg3s@gmail.com>
2025-07-25 20:01:04 +1000

    Removed upgrade button for the moment

--- src/components/Profile/ProfileModal.js
+++ src/components/Profile/ProfileModal.js
@@ -538,11 +538,12 @@ function ProfileModal({ isOpen, onClose, mainText, logout }) {
                                 placeholder="Enter your first name"
                             />
                             <p className="mt-4">You are currently on the {profile.pro ? 'Pro' : 'Free'} plan.</p>
-                            {profile.pro ? (
+                            <p>Cardify is in beta. We currently have a limited number of Pro users. If you would like to upgrade to Pro, please contact us at <a href="mailto:hello@flashcardify.app" className="text-blue-500 hover:text-blue-400">hello@flashcardify.app</a>.</p>
+                            {/* {profile.pro ? (
                                 <BackgroundButton text="Manage Billing" bgColor="bg-blue-500 hover:bg-blue-400" onClick={handleManageBillingClick} />
                             ) : (
                                 <BackgroundButton text="Upgrade to Pro" bgColor="bg-green-500 hover:bg-green-400" onClick={upgradeToPro} />
-                            )}
+                            )} */}
 
                             <p className="mt-4">Permanently delete your account</p>
                             <BackgroundButton text="Delete Account" bgColor="bg-red-500 hover:bg-red-400" onClick={() => {}} />
